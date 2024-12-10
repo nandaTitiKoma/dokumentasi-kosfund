0336375680146a8c2961d0cdb983a5424ced17be
nandaTitiKoma <rizkyabd762@gmail.com>
2024-12-10 10:22:46 +0700

    modif

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="min-h-[86.5vh] flex flex-col justify-center items-center text-center px-2 py-8">
       <h1 className="text-4xl font-bold mb-4 sm:text-7xl">Kosfund</h1>
-      <p className="max-w-[600px] text-foreground mb-8 sm:text-base">"Kelola uangmu raih impianmu"</p>
+      <p className="max-w-[600px] text-foreground mb-8 sm:text-base">'Kelola uangmu raih impianmu'</p>
       <div className="flex items-center gap-5">
         <Link href={`/docs${PageRoutes[0].href}`} className={buttonVariants({ className: "px-6", size: "lg" })}>
           Get Started
